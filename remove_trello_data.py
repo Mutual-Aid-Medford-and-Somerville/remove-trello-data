3837a0a5927edd0fd412a9b1888966c3dd7ae348
Brian Rutledge <bhrutledge@gmail.com>
2021-06-19 10:20:09 -0400

    Reformat with `black --experimental-string-processing *.py`

--- remove_trello_data.py
+++ remove_trello_data.py
@@ -1,93 +1,111 @@
-import sys, getopt
+import sys
+import getopt
 
 from trello_utils import *
 from trello_api import archiveCard, deleteCard, getCards
 
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read("config.ini")
+
 
 def prepareForArchive(card, daysSince):
-	return filterByTime(card, getPrepareArchiveDate(daysSince)) and filterByState(card, ('closed', False))
+    return filterByTime(card, getPrepareArchiveDate(daysSince)) and filterByState(
+        card, ("closed", False)
+    )
+
 
 def prepareForDelete(card, daysSince):
-	return filterByTime(card, getPrepareDeleteDate(daysSince)) and filterByState(card, ('closed', True))
+    return filterByTime(card, getPrepareDeleteDate(daysSince)) and filterByState(
+        card, ("closed", True)
+    )
+
 
 def archiveProvidedCards(cards):
-	for card in cards:
-		status = archiveCard(card['id'])
-		if status != '200':
-			print(f"Failed to archive {card['name']}.")
+    for card in cards:
+        status = archiveCard(card["id"])
+        if status != "200":
+            print(f"Failed to archive {card['name']}.")
+
 
 def deleteProvidedCards(cards):
-	for card in cards:
-		status = deleteCard(card['id'])
-		if status != '200':
-			print(f"Failed to delete {card['name']}.")
+    for card in cards:
+        status = deleteCard(card["id"])
+        if status != "200":
+            print(f"Failed to delete {card['name']}.")
+
 
 def checkActions(action, cardAmount):
-	response = input(f"Are you sure you wish to {action} {cardAmount} cards? (y/N)")
-	if(str.lower(response) == 'y'):
-		return True
-	return False
+    response = input(f"Are you sure you wish to {action} {cardAmount} cards? (y/N)")
+    if str.lower(response) == "y":
+        return True
+    return False
+
 
 def performArchiveAndDelete(action, daysSince):
-	cards = []
-	print('Getting cards from "Follow Up"...')
-	cards.extend(getCards(getFollowUpList()))
-	print('Getting cards from "Needs Met"...')
-	cards.extend(getCards(getNeedsMetList()))
+    cards = []
+    print('Getting cards from "Follow Up"...')
+    cards.extend(getCards(getFollowUpList()))
+    print('Getting cards from "Needs Met"...')
+    cards.extend(getCards(getNeedsMetList()))
 
-	if action == 'archive':
-		print('Filtering cards to archive...')
-		toArchiveList = list(filter(lambda card: prepareForArchive(card, daysSince), cards))
-		canProceed = checkActions(action, len(toArchiveList))
-		if canProceed:
-			for card in toArchiveList:
-				archiveCard(card['id'])
-		else:
-			print(f"Skipping {action} and exiting.")
-			sys.exit()
-	elif action == 'delete':
-		print('Filtering cards to delete...')
-		toDeleteList = list(filter(lambda card: prepareForDelete(card, daysSince), cards))
-		canProceed = checkActions(action, len(toDeleteList))
-		if canProceed:
-			for card in toDeleteList:
-				deleteCard(card['id'])
-		else:
-			print(f"Skipping {action} and exiting.")
-			sys.exit()
-	else:
-		print('Action unrecognized. Exiting.')
-		sys.exit()
+    if action == "archive":
+        print("Filtering cards to archive...")
+        toArchiveList = list(
+            filter(lambda card: prepareForArchive(card, daysSince), cards)
+        )
+        canProceed = checkActions(action, len(toArchiveList))
+        if canProceed:
+            for card in toArchiveList:
+                archiveCard(card["id"])
+        else:
+            print(f"Skipping {action} and exiting.")
+            sys.exit()
+    elif action == "delete":
+        print("Filtering cards to delete...")
+        toDeleteList = list(
+            filter(lambda card: prepareForDelete(card, daysSince), cards)
+        )
+        canProceed = checkActions(action, len(toDeleteList))
+        if canProceed:
+            for card in toDeleteList:
+                deleteCard(card["id"])
+        else:
+            print(f"Skipping {action} and exiting.")
+            sys.exit()
+    else:
+        print("Action unrecognized. Exiting.")
+        sys.exit()
 
-	return
+    return
+
 
 def usage():
-	print('Archive or delete Trello data.')
-	print('Usage: remove_trello_data.py -a <action> -d <days since report>')
-	print('	remove_trello_data.py -h for this dialogue.')
-	print('	Can also use --action, --days, and --help.')
-	return
+    print("Archive or delete Trello data.")
+    print("Usage: remove_trello_data.py -a <action> -d <days since report>")
+    print("	remove_trello_data.py -h for this dialogue.")
+    print("	Can also use --action, --days, and --help.")
+    return
+
 
 def main(argv):
-	action = ''
-	daysSince = 0
-	try:
-		opts, args = getopt.getopt(argv,'ha:d:',['help','action=','days='])
-	except getopt.GetoptError:
-		usage()
-		sys.exit(2)
-	for opt, arg in opts:
-		if opt in ('-h','--help'):
-			usage()
-			sys.exit()
-		elif opt in ('-a','--action'):
-			action = arg
-		elif opt in ('-d','--days'):
-			daysSince = int(arg)
+    action = ""
+    daysSince = 0
+    try:
+        opts, args = getopt.getopt(argv, "ha:d:", ["help", "action=", "days="])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif opt in ("-a", "--action"):
+            action = arg
+        elif opt in ("-d", "--days"):
+            daysSince = int(arg)
 
-	performArchiveAndDelete(action, daysSince)
+    performArchiveAndDelete(action, daysSince)
+
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
+    main(sys.argv[1:])
